13623f40aa4387b84c1c8a7dff88109d3bd2f100
Jonathan Merritt <j.s.merritt@gmail.com>
2020-09-12 15:46:48 +1000

    Testing
    From: https://os.phil-opp.com/testing/

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-extern crate rlibc;
-
-mod vga_buffer;
-
+use blog_os::println;
 use core::panic::PanicInfo;
 
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    panic!("Some panic message");
-
-    // loop {}
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info);
 }
